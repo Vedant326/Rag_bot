34743cbf078330e92665bc488dc9bea655aafd56
Vedant326 <135061848+Vedant326@users.noreply.github.com>
2024-03-06 02:31:13 +0530

    Update brain.py

--- brain.py
+++ brain.py
@@ -9,7 +9,7 @@ from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores.faiss import FAISS
 from pypdf import PdfReader
-#import faiss
+import faiss
 
 
 def parse_pdf(file: BytesIO, filename: str) -> Tuple[List[str], str]:
